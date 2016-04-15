aa9842c654b1ac9e9101e2bc1c750adf87c4d416
RowanZee <rowan.ziman@gmail.com>
2016-04-15 11:25:28 +0200

    Update users.js

--- static/js/users.js
+++ static/js/users.js
@@ -7,15 +7,15 @@ $(document).ready(function () {
     		$('.date-time').removeClass("hidden");
     		var date = new Date();
 
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
+            var day = date.getDate();
+            var month = date.getMonth() + 1;
+            var year = date.getFullYear();
 
-    if (month < 10) month = "0" + month;
-    if (day < 10) day = "0" + day;
+            if (month < 10) month = "0" + month;
+            if (day < 10) day = "0" + day;
 
-    var today = year + "-" + month + "-" + day;       
-    $("#tempDate").attr("value", today);
+            var today = year + "-" + month + "-" + day;       
+            $("#tempDate").attr("value", "2011-01-01");
 
         } else {
             $('.date-time').addClass("hidden");
