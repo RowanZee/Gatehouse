5626f34861739daa586da5b1b1ccf2dfe0fa4eef
RowanZee <rowan.ziman@gmail.com>
2016-04-17 18:27:47 +0200

    indent error

--- modules/database_model.py
+++ modules/database_model.py
@@ -13,8 +13,7 @@ class UserModel(db.Model):
     password = db.Column(db.String(120), unique=True)
     admin = db.Column(db.Boolean())
     experationDate = db.Column(db.String(80))
-	weekday = db.relationship('weekDay', backref="usermodel", cascade="all, delete-orphan", lazy='dynamic')
-
+	weekday = db.relationship('weekDay', backref="usermodel", lazy='dynamic')
 
     def __init__(self, username, password, admin, experationDate):
         self.username = username
@@ -28,7 +27,6 @@ class weekDay(db.Model):
 	#Defining the Foreign Key on the Child Table
 	dayname = db.Column(db.String(15))
 	usermodel_id = db.Column(db.Integer, db.ForeignKey('UserModel.id'))
-	
 	
 	 def __init__(self, dayname):
         self.dayname = dayname
