a8ce4616b851895e0e7bafd536af8c1853301803
RowanZee <rowan.ziman@gmail.com>
2016-05-02 22:41:30 +0200

    error message test

--- server.py
+++ server.py
@@ -336,7 +336,7 @@ def toggledoor():
         return redirect(url_for('login'))
 
     username = request.form['username']
-    return render_template('index.html', error="RIWAN")
+    return redirect(url_for('index', error="ROWAN"))
 
     # Toggles Door
     #garage.toggleDoor()
