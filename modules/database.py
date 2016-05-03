e0258d0dbfe2aa927fd09ea1e40c0fca940f51c5
RowanZee <rowan.ziman@gmail.com>
2016-05-03 21:27:14 +0200

    test query

--- modules/database.py
+++ modules/database.py
@@ -92,5 +92,6 @@ class Database:
  
     # LIST DAYS FOR USER
     def getUserDays(self):
-        days = weekDay.query.filter_by(dayname='Monday').all()
+        #days = weekDay.query.filter_by(dayname='Monday').all()
+		days = UserModel.query.filter(UserModel.weekday.any(dayname="Monday'))
         return days
