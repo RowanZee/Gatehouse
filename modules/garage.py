17fd04c7f24254a55fb15d236eb56c4bc199c557
RowanZee <rowan.ziman@gmail.com>
2016-05-17 21:24:25 +0200

    rpio gpio

--- modules/garage.py
+++ modules/garage.py
@@ -1,6 +1,5 @@
 try:
-    #import RPi.GPIO as GPIO
-    import wiringpi
+    import RPi.GPIO as GPIO
 except:
     print("Notice: Not Connected To A Pi.")
 import time
@@ -11,9 +10,7 @@ class Garage:
 
     def __init__(self):
         try:
-            io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
-            #GPIO.setmode(GPIO.BCM)
-            io.pinMode(4,io.OUTPUT) # Setup pin 22 (GPIO25)
+            GPIO.setmode(GPIO.BCM)
             self.cleanupRelay()
         except:
             print("Warning: Could Not Init Gate.")
@@ -22,12 +19,11 @@ class Garage:
     def toggleDoor(self):
         try:
             for pin in self.pinList:
-                io.pinMode(4,io.OUTPUT) # Setup pin 22 (GPIO25)
-                #GPIO.output(pin, GPIO.HIGH)
+                GPIO.output(pin, GPIO.HIGH)
 
             time.sleep(.2)
-            #GPIO.cleanup()
-            #GPIO.setmode(GPIO.BCM)
+            GPIO.cleanup()
+            GPIO.setmode(GPIO.BCM)
             self.cleanupRelay()
         except:
             print("Warning: Failed To Toggle Door.")
@@ -36,8 +32,7 @@ class Garage:
     def cleanupRelay(self):
         try:
             for pin in self.pinList:
-                #GPIO.setup(pin, GPIO.OUT)
-                io.digitalWrite(pin,io.LOW) 
-                #GPIO.output(pin, GPIO.LOW)
+                GPIO.setup(pin, GPIO.OUT)
+                GPIO.output(pin, GPIO.LOW)
         except:
             print("Warning: Failed To Clean Up.")
